5deec40be496ec11c42fdf1f344a2d321d020dfe
Thauweer <thauweer@yandex.ru>
2024-07-24 16:29:17 +0400

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'build')
     },
     plugins: [
         new HtmlWebpackPlugin({
